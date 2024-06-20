4f81461a224b5f80015bbdea4beec41c651d444c
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-06-20 22:33:17 +0900

    ✨ Pipe를 이용한 유효성 검사
    Board 부분 Pipe를 이용한 유효성 검사

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateBoardDto {
+  @IsNotEmpty()
   title: String;
+
+  @IsNotEmpty()
   description: String;
 }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../boards.model';
+
+export class BoardStatusValidationPiple implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
