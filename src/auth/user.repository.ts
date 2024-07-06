4e1f17123c32d928e7f96e449a262b2d4cc8b6d4
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-07-07 03:20:07 +0900

    ✨ Feat. 회원가입 구현
    회원가입 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,35 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing usernmae');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
